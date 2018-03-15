2bce3a92138740337f09802251c5e7b8f1727d5a
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 22:40:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function driverNamesWithRevenueOver(obj, rev) {
 }
 
 function exactMatch(obj, search) {
-  return obj.filter(function(driver) { return driver.name === search.name })
+  return obj.filter(function(driver) { return driver.name.toLowerCase() === search.name.toLowerCase() })
 }
