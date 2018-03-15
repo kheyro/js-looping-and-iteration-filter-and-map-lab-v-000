74a80bc71f052026a39e3aa935114072b0daf4ca
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 22:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 function driversWithRevenueOver(obj, rev) {
   return obj.filter(function(driver) { return driver.revenue > rev })
 }
+
+function driverNamesWithRevenueOver(obj, rev) {
+  let newObj = driversWithRevenueOver(obj, rev)
+  return newObj.map(
+    function (driver) {
+      return driver.name
+    })
+}
